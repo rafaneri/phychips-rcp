9a78c18de3fcb8c86c700b588cfb46a5ac268fc5
Rafael da Mata Neri <rafael.neri@gmail.com>
2018-06-07 02:26:46 -0300

    Removed length parameter

--- test/crc16.test.ts
+++ test/crc16.test.ts
@@ -3,7 +3,7 @@ import { crc16 } from '../lib/crc16';
 
 describe('crc16 function test', () => {
     it('should be a 0', () => {
-        const result = crc16(Buffer.from([0x00]), -1, 0);
+        const result = crc16(Buffer.from([0x00]), -1);
         expect(result).to.equals(0);
     });
 });
