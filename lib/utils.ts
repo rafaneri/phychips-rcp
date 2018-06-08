f56f3533e7c11c9dbf45f36261123a75be12a0ff
Rafael da Mata Neri <rafael.neri@gmail.com>
2018-06-08 01:15:31 -0300

    Adjust the tobytearray function to add 0 before when the string is  odd

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,11 @@ export module Util {
 
     export function toByteArray(hexString: string): number[] {
         var result = [];
+
+        while (hexString.length % 2 != 0) {
+            hexString = '0' + hexString;
+        }
+
         while (hexString.length >= 2) {
             result.push(parseInt(hexString.substring(0, 2), 16));
             hexString = hexString.substring(2, hexString.length);
